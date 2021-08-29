89b2aea760ce97699d8d4fe870ca6b8604584bfd
Jadson <jcsc@ic.ufal.br>
2021-08-29 13:54:54 -0300

    create CRUD

---
+++ app/sql_app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def get_images(db: Session):
+    query = db.query(models.Image).all()
+    print(query)
+    return query
+
+
+def get_image(db: Session, image_id: int):
+    return db.query(models.Image).filter(models.Image.id == image_id).first()
+
+
+def delete_image(db: Session, image_id: int):
+    if image := get_image(db, image_id):
+        db.delete(image)
+        db.commit()
+        return True
+
+    return False
+
+
+def create_image(db: Session, image: schemas.Image):
+    new_image = models.Image(image_input=image.image_input,
+                          image_output=image.image_output,
+                          ocr = image.ocr)
+    db.add(new_image)
+    db.commit()
+    db.refresh(new_image)
+    return new_image
